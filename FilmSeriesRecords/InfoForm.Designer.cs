4b71ae807164d06b3cdebbada10ffbb624856c4b
iArmann <mrevid2@gmail.com>
2021-02-12 01:40:34 +0330

    Designed info form UI

--- FilmSeriesRecords/InfoForm.Designer.cs
+++ FilmSeriesRecords/InfoForm.Designer.cs
@@ -29,20 +29,315 @@ namespace FilmSeriesRecords
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InfoForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtboxName = new System.Windows.Forms.TextBox();
+			this.txtboxSeasons = new System.Windows.Forms.TextBox();
+			this.txtboxStatus = new System.Windows.Forms.TextBox();
+			this.btnFavorite = new System.Windows.Forms.Button();
+			this.groupBoxSchedule = new System.Windows.Forms.GroupBox();
+			this.txtboxScheduleInterruptionTime = new System.Windows.Forms.TextBox();
+			this.txtboxScheduleStartsAtTime = new System.Windows.Forms.TextBox();
+			this.txtboxScheduleStartsAtDate = new System.Windows.Forms.TextBox();
+			this.txtboxScheduleEpisode = new System.Windows.Forms.TextBox();
+			this.txtboxScheduleSeason = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.txtboxDetailTimesWatched = new System.Windows.Forms.TextBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.richTextBoxDescription = new System.Windows.Forms.RichTextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.richTextBoxGenres = new System.Windows.Forms.RichTextBox();
+			this.groupBoxSchedule.SuspendLayout();
+			this.groupBox1.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(108, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(49, 17);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Name:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(286, 48);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(67, 17);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Seasons:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(105, 46);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(52, 17);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Status:";
+			// 
+			// txtboxName
+			// 
+			this.txtboxName.Location = new System.Drawing.Point(163, 9);
+			this.txtboxName.Name = "txtboxName";
+			this.txtboxName.ReadOnly = true;
+			this.txtboxName.Size = new System.Drawing.Size(294, 22);
+			this.txtboxName.TabIndex = 4;
+			// 
+			// txtboxSeasons
+			// 
+			this.txtboxSeasons.Location = new System.Drawing.Point(359, 45);
+			this.txtboxSeasons.Name = "txtboxSeasons";
+			this.txtboxSeasons.ReadOnly = true;
+			this.txtboxSeasons.Size = new System.Drawing.Size(82, 22);
+			this.txtboxSeasons.TabIndex = 5;
+			// 
+			// txtboxStatus
+			// 
+			this.txtboxStatus.Location = new System.Drawing.Point(163, 45);
+			this.txtboxStatus.Name = "txtboxStatus";
+			this.txtboxStatus.ReadOnly = true;
+			this.txtboxStatus.Size = new System.Drawing.Size(106, 22);
+			this.txtboxStatus.TabIndex = 6;
+			// 
+			// btnFavorite
+			// 
+			this.btnFavorite.BackColor = System.Drawing.Color.Transparent;
+			this.btnFavorite.FlatAppearance.BorderSize = 0;
+			this.btnFavorite.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnFavorite.Image = ((System.Drawing.Image)(resources.GetObject("btnFavorite.Image")));
+			this.btnFavorite.Location = new System.Drawing.Point(463, 22);
+			this.btnFavorite.Name = "btnFavorite";
+			this.btnFavorite.Size = new System.Drawing.Size(37, 41);
+			this.btnFavorite.TabIndex = 16;
+			this.btnFavorite.UseVisualStyleBackColor = false;
+			// 
+			// groupBoxSchedule
+			// 
+			this.groupBoxSchedule.Controls.Add(this.txtboxScheduleInterruptionTime);
+			this.groupBoxSchedule.Controls.Add(this.txtboxScheduleStartsAtTime);
+			this.groupBoxSchedule.Controls.Add(this.txtboxScheduleStartsAtDate);
+			this.groupBoxSchedule.Controls.Add(this.txtboxScheduleEpisode);
+			this.groupBoxSchedule.Controls.Add(this.txtboxScheduleSeason);
+			this.groupBoxSchedule.Controls.Add(this.label7);
+			this.groupBoxSchedule.Controls.Add(this.label6);
+			this.groupBoxSchedule.Controls.Add(this.label5);
+			this.groupBoxSchedule.Controls.Add(this.label4);
+			this.groupBoxSchedule.Location = new System.Drawing.Point(12, 85);
+			this.groupBoxSchedule.Name = "groupBoxSchedule";
+			this.groupBoxSchedule.Size = new System.Drawing.Size(294, 217);
+			this.groupBoxSchedule.TabIndex = 17;
+			this.groupBoxSchedule.TabStop = false;
+			this.groupBoxSchedule.Text = "Next show schedule";
+			// 
+			// txtboxScheduleInterruptionTime
+			// 
+			this.txtboxScheduleInterruptionTime.Location = new System.Drawing.Point(126, 99);
+			this.txtboxScheduleInterruptionTime.Name = "txtboxScheduleInterruptionTime";
+			this.txtboxScheduleInterruptionTime.ReadOnly = true;
+			this.txtboxScheduleInterruptionTime.Size = new System.Drawing.Size(112, 22);
+			this.txtboxScheduleInterruptionTime.TabIndex = 18;
+			// 
+			// txtboxScheduleStartsAtTime
+			// 
+			this.txtboxScheduleStartsAtTime.Location = new System.Drawing.Point(77, 176);
+			this.txtboxScheduleStartsAtTime.Name = "txtboxScheduleStartsAtTime";
+			this.txtboxScheduleStartsAtTime.ReadOnly = true;
+			this.txtboxScheduleStartsAtTime.Size = new System.Drawing.Size(128, 22);
+			this.txtboxScheduleStartsAtTime.TabIndex = 17;
+			// 
+			// txtboxScheduleStartsAtDate
+			// 
+			this.txtboxScheduleStartsAtDate.Location = new System.Drawing.Point(77, 148);
+			this.txtboxScheduleStartsAtDate.Name = "txtboxScheduleStartsAtDate";
+			this.txtboxScheduleStartsAtDate.ReadOnly = true;
+			this.txtboxScheduleStartsAtDate.Size = new System.Drawing.Size(210, 22);
+			this.txtboxScheduleStartsAtDate.TabIndex = 16;
+			// 
+			// txtboxScheduleEpisode
+			// 
+			this.txtboxScheduleEpisode.Location = new System.Drawing.Point(69, 60);
+			this.txtboxScheduleEpisode.Name = "txtboxScheduleEpisode";
+			this.txtboxScheduleEpisode.ReadOnly = true;
+			this.txtboxScheduleEpisode.Size = new System.Drawing.Size(82, 22);
+			this.txtboxScheduleEpisode.TabIndex = 15;
+			// 
+			// txtboxScheduleSeason
+			// 
+			this.txtboxScheduleSeason.Location = new System.Drawing.Point(69, 26);
+			this.txtboxScheduleSeason.Name = "txtboxScheduleSeason";
+			this.txtboxScheduleSeason.ReadOnly = true;
+			this.txtboxScheduleSeason.Size = new System.Drawing.Size(82, 22);
+			this.txtboxScheduleSeason.TabIndex = 14;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(6, 149);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(65, 17);
+			this.label7.TabIndex = 4;
+			this.label7.Text = "Starts at:";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(6, 99);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(114, 17);
+			this.label6.TabIndex = 3;
+			this.label6.Text = "Interruption time:";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(6, 61);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(63, 17);
+			this.label5.TabIndex = 2;
+			this.label5.Text = "Episode:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(6, 29);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(60, 17);
+			this.label4.TabIndex = 1;
+			this.label4.Text = "Season:";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.txtboxDetailTimesWatched);
+			this.groupBox1.Controls.Add(this.label10);
+			this.groupBox1.Controls.Add(this.richTextBoxDescription);
+			this.groupBox1.Controls.Add(this.label9);
+			this.groupBox1.Controls.Add(this.label8);
+			this.groupBox1.Controls.Add(this.richTextBoxGenres);
+			this.groupBox1.Location = new System.Drawing.Point(312, 85);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(282, 217);
+			this.groupBox1.TabIndex = 18;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Detail";
+			// 
+			// txtboxDetailTimesWatched
+			// 
+			this.txtboxDetailTimesWatched.Location = new System.Drawing.Point(187, 21);
+			this.txtboxDetailTimesWatched.Name = "txtboxDetailTimesWatched";
+			this.txtboxDetailTimesWatched.ReadOnly = true;
+			this.txtboxDetailTimesWatched.Size = new System.Drawing.Size(82, 22);
+			this.txtboxDetailTimesWatched.TabIndex = 6;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(13, 24);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(171, 17);
+			this.label10.TabIndex = 5;
+			this.label10.Text = "Number of times watched:";
+			// 
+			// richTextBoxDescription
+			// 
+			this.richTextBoxDescription.Location = new System.Drawing.Point(16, 136);
+			this.richTextBoxDescription.Name = "richTextBoxDescription";
+			this.richTextBoxDescription.ReadOnly = true;
+			this.richTextBoxDescription.Size = new System.Drawing.Size(253, 68);
+			this.richTextBoxDescription.TabIndex = 4;
+			this.richTextBoxDescription.Text = "";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(13, 116);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(83, 17);
+			this.label9.TabIndex = 3;
+			this.label9.Text = "Description:";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(13, 48);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(59, 17);
+			this.label8.TabIndex = 2;
+			this.label8.Text = "Genres:";
+			// 
+			// richTextBoxGenres
+			// 
+			this.richTextBoxGenres.Location = new System.Drawing.Point(16, 68);
+			this.richTextBoxGenres.Name = "richTextBoxGenres";
+			this.richTextBoxGenres.ReadOnly = true;
+			this.richTextBoxGenres.Size = new System.Drawing.Size(253, 35);
+			this.richTextBoxGenres.TabIndex = 0;
+			this.richTextBoxGenres.Text = "";
 			// 
 			// InfoForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(605, 314);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.groupBoxSchedule);
+			this.Controls.Add(this.btnFavorite);
+			this.Controls.Add(this.txtboxStatus);
+			this.Controls.Add(this.txtboxSeasons);
+			this.Controls.Add(this.txtboxName);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.KeyPreview = true;
 			this.Name = "InfoForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "InfoForm";
+			this.Text = "Info";
+			this.Shown += new System.EventHandler(this.InfoForm_Shown);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.InfoForm_KeyDown);
+			this.groupBoxSchedule.ResumeLayout(false);
+			this.groupBoxSchedule.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtboxName;
+		private System.Windows.Forms.TextBox txtboxSeasons;
+		private System.Windows.Forms.TextBox txtboxStatus;
+		private System.Windows.Forms.Button btnFavorite;
+		private System.Windows.Forms.GroupBox groupBoxSchedule;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox txtboxScheduleEpisode;
+		private System.Windows.Forms.TextBox txtboxScheduleSeason;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.RichTextBox richTextBoxDescription;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.RichTextBox richTextBoxGenres;
+		private System.Windows.Forms.TextBox txtboxDetailTimesWatched;
+		private System.Windows.Forms.TextBox txtboxScheduleStartsAtTime;
+		private System.Windows.Forms.TextBox txtboxScheduleStartsAtDate;
+		private System.Windows.Forms.TextBox txtboxScheduleInterruptionTime;
 	}
 }
