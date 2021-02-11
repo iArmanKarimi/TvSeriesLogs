4924fec757fbfc51fe5c0f0f6d334efe1ffcc395
iArmann <mrevid2@gmail.com>
2021-02-11 06:47:36 +0330

    Changed Genres' type from string[] to string

--- FilmSeriesRecordsDB/SeriesDetail.cs
+++ FilmSeriesRecordsDB/SeriesDetail.cs
@@ -3,7 +3,7 @@
 	public class SeriesDetail
 	{
 		public bool IsFavorite { get; set; }
-		public string[] Genres { get; set; }
+		public string Genres { get; set; }
 		public string Description { get; set; }
 		public ushort TimesWatched { get; set; } // How many times was this series seen
 	}
