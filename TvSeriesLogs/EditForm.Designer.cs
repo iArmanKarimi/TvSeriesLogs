48620f7466ce8903db02fb79f51202f63cdf7062
iArmann <mrevid2@gmail.com>
2021-04-07 22:30:40 +0430

    Updated UI

--- TvSeriesLogs/EditForm.Designer.cs
+++ TvSeriesLogs/EditForm.Designer.cs
@@ -70,7 +70,7 @@ namespace TvSeriesLogs
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(130, 15);
+			this.label1.Location = new System.Drawing.Point(12, 9);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(49, 17);
 			this.label1.TabIndex = 0;
@@ -78,15 +78,15 @@ namespace TvSeriesLogs
 			// 
 			// txtboxName
 			// 
-			this.txtboxName.Location = new System.Drawing.Point(180, 14);
+			this.txtboxName.Location = new System.Drawing.Point(62, 8);
 			this.txtboxName.Name = "txtboxName";
-			this.txtboxName.Size = new System.Drawing.Size(307, 22);
+			this.txtboxName.Size = new System.Drawing.Size(334, 22);
 			this.txtboxName.TabIndex = 1;
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(130, 51);
+			this.label2.Location = new System.Drawing.Point(12, 49);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(52, 17);
 			this.label2.TabIndex = 2;
@@ -104,7 +104,7 @@ namespace TvSeriesLogs
             "haven\'t watched",
             "watching",
             "watched"});
-			this.comboBoxStatus.Location = new System.Drawing.Point(180, 50);
+			this.comboBoxStatus.Location = new System.Drawing.Point(62, 48);
 			this.comboBoxStatus.Name = "comboBoxStatus";
 			this.comboBoxStatus.Size = new System.Drawing.Size(127, 24);
 			this.comboBoxStatus.TabIndex = 2;
@@ -112,16 +112,16 @@ namespace TvSeriesLogs
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(335, 51);
+			this.label3.Location = new System.Drawing.Point(207, 51);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(67, 17);
+			this.label3.Size = new System.Drawing.Size(101, 17);
 			this.label3.TabIndex = 8;
-			this.label3.Text = "Seasons:";
+			this.label3.Text = "Total seasons:";
 			// 
 			// numericUpDownSeasons
 			// 
 			this.numericUpDownSeasons.Font = new System.Drawing.Font("Fira Code", 9F);
-			this.numericUpDownSeasons.Location = new System.Drawing.Point(404, 49);
+			this.numericUpDownSeasons.Location = new System.Drawing.Point(314, 47);
 			this.numericUpDownSeasons.Maximum = new decimal(new int[] {
             65535,
             0,
@@ -133,14 +133,15 @@ namespace TvSeriesLogs
 			// 
 			// btnSave
 			// 
+			this.btnSave.BackColor = System.Drawing.Color.Transparent;
 			this.btnSave.FlatAppearance.BorderSize = 0;
 			this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
-			this.btnSave.Location = new System.Drawing.Point(295, 313);
+			this.btnSave.Location = new System.Drawing.Point(480, 12);
 			this.btnSave.Name = "btnSave";
-			this.btnSave.Size = new System.Drawing.Size(63, 52);
+			this.btnSave.Size = new System.Drawing.Size(93, 76);
 			this.btnSave.TabIndex = 11;
-			this.btnSave.UseVisualStyleBackColor = true;
+			this.btnSave.UseVisualStyleBackColor = false;
 			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
 			// 
 			// groupBoxSchedule
@@ -158,10 +159,10 @@ namespace TvSeriesLogs
 			this.groupBoxSchedule.Controls.Add(this.label4);
 			this.groupBoxSchedule.Location = new System.Drawing.Point(12, 90);
 			this.groupBoxSchedule.Name = "groupBoxSchedule";
-			this.groupBoxSchedule.Size = new System.Drawing.Size(336, 217);
+			this.groupBoxSchedule.Size = new System.Drawing.Size(311, 251);
 			this.groupBoxSchedule.TabIndex = 12;
 			this.groupBoxSchedule.TabStop = false;
-			this.groupBoxSchedule.Text = "Next show schedule";
+			this.groupBoxSchedule.Text = "Scheduling";
 			// 
 			// btnResetScheduleStartsAt
 			// 
@@ -169,9 +170,9 @@ namespace TvSeriesLogs
 			this.btnResetScheduleStartsAt.FlatAppearance.BorderSize = 0;
 			this.btnResetScheduleStartsAt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnResetScheduleStartsAt.Image = ((System.Drawing.Image)(resources.GetObject("btnResetScheduleStartsAt.Image")));
-			this.btnResetScheduleStartsAt.Location = new System.Drawing.Point(214, 174);
+			this.btnResetScheduleStartsAt.Location = new System.Drawing.Point(243, 213);
 			this.btnResetScheduleStartsAt.Name = "btnResetScheduleStartsAt";
-			this.btnResetScheduleStartsAt.Size = new System.Drawing.Size(42, 23);
+			this.btnResetScheduleStartsAt.Size = new System.Drawing.Size(53, 23);
 			this.btnResetScheduleStartsAt.TabIndex = 13;
 			this.btnResetScheduleStartsAt.UseVisualStyleBackColor = false;
 			this.btnResetScheduleStartsAt.Click += new System.EventHandler(this.btnResetScheduleStartsAt_Click);
@@ -182,9 +183,9 @@ namespace TvSeriesLogs
 			this.btnResetScheduleInterruptionTime.FlatAppearance.BorderSize = 0;
 			this.btnResetScheduleInterruptionTime.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnResetScheduleInterruptionTime.Image = ((System.Drawing.Image)(resources.GetObject("btnResetScheduleInterruptionTime.Image")));
-			this.btnResetScheduleInterruptionTime.Location = new System.Drawing.Point(229, 107);
+			this.btnResetScheduleInterruptionTime.Location = new System.Drawing.Point(237, 114);
 			this.btnResetScheduleInterruptionTime.Name = "btnResetScheduleInterruptionTime";
-			this.btnResetScheduleInterruptionTime.Size = new System.Drawing.Size(42, 23);
+			this.btnResetScheduleInterruptionTime.Size = new System.Drawing.Size(48, 23);
 			this.btnResetScheduleInterruptionTime.TabIndex = 11;
 			this.btnResetScheduleInterruptionTime.UseVisualStyleBackColor = false;
 			this.btnResetScheduleInterruptionTime.Click += new System.EventHandler(this.btnResetScheduleInterruptionTime_Click);
@@ -192,7 +193,7 @@ namespace TvSeriesLogs
 			// dateTimePickerShowStartsAtTime
 			// 
 			this.dateTimePickerShowStartsAtTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-			this.dateTimePickerShowStartsAtTime.Location = new System.Drawing.Point(69, 175);
+			this.dateTimePickerShowStartsAtTime.Location = new System.Drawing.Point(98, 211);
 			this.dateTimePickerShowStartsAtTime.Name = "dateTimePickerShowStartsAtTime";
 			this.dateTimePickerShowStartsAtTime.ShowUpDown = true;
 			this.dateTimePickerShowStartsAtTime.Size = new System.Drawing.Size(139, 22);
@@ -201,7 +202,7 @@ namespace TvSeriesLogs
 			// 
 			// dateTimePickerShowStartsAtDate
 			// 
-			this.dateTimePickerShowStartsAtDate.Location = new System.Drawing.Point(69, 147);
+			this.dateTimePickerShowStartsAtDate.Location = new System.Drawing.Point(42, 183);
 			this.dateTimePickerShowStartsAtDate.Name = "dateTimePickerShowStartsAtDate";
 			this.dateTimePickerShowStartsAtDate.Size = new System.Drawing.Size(254, 22);
 			this.dateTimePickerShowStartsAtDate.TabIndex = 9;
@@ -210,7 +211,7 @@ namespace TvSeriesLogs
 			// 
 			this.dateTimePickerInterruptionTime.CustomFormat = "HH:mm:ss";
 			this.dateTimePickerInterruptionTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-			this.dateTimePickerInterruptionTime.Location = new System.Drawing.Point(118, 107);
+			this.dateTimePickerInterruptionTime.Location = new System.Drawing.Point(126, 115);
 			this.dateTimePickerInterruptionTime.Name = "dateTimePickerInterruptionTime";
 			this.dateTimePickerInterruptionTime.ShowUpDown = true;
 			this.dateTimePickerInterruptionTime.Size = new System.Drawing.Size(105, 22);
@@ -219,43 +220,43 @@ namespace TvSeriesLogs
 			// 
 			// numericUpDownScheduleEpisode
 			// 
-			this.numericUpDownScheduleEpisode.Font = new System.Drawing.Font("Fira Code", 9F);
-			this.numericUpDownScheduleEpisode.Location = new System.Drawing.Point(72, 58);
+			this.numericUpDownScheduleEpisode.Font = new System.Drawing.Font("Consolas", 10.2F);
+			this.numericUpDownScheduleEpisode.Location = new System.Drawing.Point(123, 68);
 			this.numericUpDownScheduleEpisode.Maximum = new decimal(new int[] {
             65535,
             0,
             0,
             0});
 			this.numericUpDownScheduleEpisode.Name = "numericUpDownScheduleEpisode";
-			this.numericUpDownScheduleEpisode.Size = new System.Drawing.Size(82, 26);
+			this.numericUpDownScheduleEpisode.Size = new System.Drawing.Size(82, 27);
 			this.numericUpDownScheduleEpisode.TabIndex = 5;
 			// 
 			// numericUpDownScheduleSeasons
 			// 
-			this.numericUpDownScheduleSeasons.Font = new System.Drawing.Font("Fira Code", 9F);
-			this.numericUpDownScheduleSeasons.Location = new System.Drawing.Point(72, 26);
+			this.numericUpDownScheduleSeasons.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.numericUpDownScheduleSeasons.Location = new System.Drawing.Point(123, 30);
 			this.numericUpDownScheduleSeasons.Maximum = new decimal(new int[] {
             65535,
             0,
             0,
             0});
 			this.numericUpDownScheduleSeasons.Name = "numericUpDownScheduleSeasons";
-			this.numericUpDownScheduleSeasons.Size = new System.Drawing.Size(82, 26);
+			this.numericUpDownScheduleSeasons.Size = new System.Drawing.Size(82, 27);
 			this.numericUpDownScheduleSeasons.TabIndex = 4;
 			// 
 			// label7
 			// 
 			this.label7.AutoSize = true;
-			this.label7.Location = new System.Drawing.Point(6, 149);
+			this.label7.Location = new System.Drawing.Point(6, 153);
 			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(65, 17);
+			this.label7.Size = new System.Drawing.Size(137, 17);
 			this.label7.TabIndex = 4;
-			this.label7.Text = "Starts at:";
+			this.label7.Text = "See next episode at:";
 			// 
 			// label6
 			// 
 			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(6, 107);
+			this.label6.Location = new System.Drawing.Point(6, 115);
 			this.label6.Name = "label6";
 			this.label6.Size = new System.Drawing.Size(114, 17);
 			this.label6.TabIndex = 3;
@@ -264,7 +265,7 @@ namespace TvSeriesLogs
 			// label5
 			// 
 			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(6, 61);
+			this.label5.Location = new System.Drawing.Point(57, 71);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(63, 17);
 			this.label5.TabIndex = 2;
@@ -273,7 +274,7 @@ namespace TvSeriesLogs
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(6, 29);
+			this.label4.Location = new System.Drawing.Point(57, 35);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(60, 17);
 			this.label4.TabIndex = 1;
@@ -292,24 +293,24 @@ namespace TvSeriesLogs
 			this.groupBox1.Controls.Add(this.label9);
 			this.groupBox1.Controls.Add(this.label8);
 			this.groupBox1.Controls.Add(this.richTextBoxGenres);
-			this.groupBox1.Location = new System.Drawing.Point(354, 90);
+			this.groupBox1.Location = new System.Drawing.Point(329, 90);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(282, 217);
+			this.groupBox1.Size = new System.Drawing.Size(282, 251);
 			this.groupBox1.TabIndex = 14;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Detail";
 			// 
 			// numericUpDownTimesWatched
 			// 
-			this.numericUpDownTimesWatched.Font = new System.Drawing.Font("Fira Code", 9F);
-			this.numericUpDownTimesWatched.Location = new System.Drawing.Point(187, 21);
+			this.numericUpDownTimesWatched.Font = new System.Drawing.Font("Consolas", 10.2F);
+			this.numericUpDownTimesWatched.Location = new System.Drawing.Point(187, 20);
 			this.numericUpDownTimesWatched.Maximum = new decimal(new int[] {
             65535,
             0,
             0,
             0});
 			this.numericUpDownTimesWatched.Name = "numericUpDownTimesWatched";
-			this.numericUpDownTimesWatched.Size = new System.Drawing.Size(82, 26);
+			this.numericUpDownTimesWatched.Size = new System.Drawing.Size(82, 27);
 			this.numericUpDownTimesWatched.TabIndex = 6;
 			// 
 			// label10
@@ -325,7 +326,7 @@ namespace TvSeriesLogs
 			// 
 			this.richTextBoxDescription.Location = new System.Drawing.Point(16, 136);
 			this.richTextBoxDescription.Name = "richTextBoxDescription";
-			this.richTextBoxDescription.Size = new System.Drawing.Size(253, 68);
+			this.richTextBoxDescription.Size = new System.Drawing.Size(253, 109);
 			this.richTextBoxDescription.TabIndex = 4;
 			this.richTextBoxDescription.Text = "";
 			// 
@@ -361,9 +362,9 @@ namespace TvSeriesLogs
 			this.btnFavorite.FlatAppearance.BorderSize = 0;
 			this.btnFavorite.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnFavorite.Image = ((System.Drawing.Image)(resources.GetObject("btnFavorite.Image")));
-			this.btnFavorite.Location = new System.Drawing.Point(493, 20);
+			this.btnFavorite.Location = new System.Drawing.Point(403, 12);
 			this.btnFavorite.Name = "btnFavorite";
-			this.btnFavorite.Size = new System.Drawing.Size(47, 48);
+			this.btnFavorite.Size = new System.Drawing.Size(50, 61);
 			this.btnFavorite.TabIndex = 15;
 			this.btnFavorite.UseVisualStyleBackColor = false;
 			this.btnFavorite.Click += new System.EventHandler(this.btnFavorite_Click);
@@ -372,7 +373,7 @@ namespace TvSeriesLogs
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(649, 373);
+			this.ClientSize = new System.Drawing.Size(619, 348);
 			this.Controls.Add(this.btnFavorite);
 			this.Controls.Add(this.groupBox1);
 			this.Controls.Add(this.label3);
